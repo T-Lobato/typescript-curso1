af9286bca646fbb7813039dd54a8d930a5aad1b8
Thyago Lobato <thyagollobato@gmail.com>
2023-04-25 18:01:40 -0300

    Fim do projeto

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,15 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = []; // Forma simplificada de declaracao de Array em Typescript. 
+    // private negociacoes: Array<Negociacao> = []; Forma mais comum de declaração de Array.
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    lista(): ReadonlyArray<Negociacao> { // Forma mais comum de declaração de ReadOnlyArray.
+    // lista(): readonly Negociacao[] { Forma simplificada de declaração de ReadOnlyArray.   
         return this.negociacoes;
     }
 
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,11 +1,13 @@
 export class Negociacoes {
     constructor() {
-        this.negociacoes = [];
+        this.negociacoes = []; // Forma simplificada de declaracao de Array em Typescript. 
     }
+    // private negociacoes: Array<Negociacao> = []; Forma mais comum de declaração de Array.
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
     lista() {
+        // lista(): readonly Negociacao[] { Forma simplificada de declaração de ReadOnlyArray.   
         return this.negociacoes;
     }
 }
