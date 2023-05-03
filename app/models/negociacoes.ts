3ac3811edbe175e84c601890ef123859789b26e8
Thyago Lobato <thyagollobato@gmail.com>
2023-05-03 20:59:43 -0300

    Visibilidade de métodos e Enumeration

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -4,11 +4,11 @@ export class Negociacoes {
     private negociacoes: Negociacao[] = []; // Forma simplificada de declaracao de Array em Typescript. 
     // private negociacoes: Array<Negociacao> = []; Forma mais comum de declaração de Array.
 
-    adiciona(negociacao: Negociacao) {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao> { // Forma mais comum de declaração de ReadOnlyArray.
+    public lista(): ReadonlyArray<Negociacao> { // Forma mais comum de declaração de ReadOnlyArray.
     // lista(): readonly Negociacao[] { Forma simplificada de declaração de ReadOnlyArray.   
         return this.negociacoes;
     }
